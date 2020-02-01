406c1f7c360c0930a2098b334ad8a3e408bd575a
Davide <davide.rolando@gmail.com>
2020-02-01 17:39:07 +0100

    New feature: copy code text to clipboard

--- action.php
+++ action.php
@@ -3,6 +3,8 @@
  * Action Plugin: Inserts a button into the toolbar to add file tags
  *
  * @author Georg Schmidt, Heiko Barth
+ * @mod Davide Rolando
+ * Default cache time DokuWiki: 60*60*24 (https://www.dokuwiki.org/config:cachetime)
  */
  
 if (!defined('DOKU_INC')) die();
@@ -16,6 +18,8 @@ class action_plugin_codebuttonmod1 extends DokuWiki_Action_Plugin {
      */
     function register(Doku_Event_Handler $controller) {
         $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array ());
+		$controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button_copy', array ());
+		$controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this,'_hookjs');
     }
  
     /**
@@ -25,9 +29,37 @@ class action_plugin_codebuttonmod1 extends DokuWiki_Action_Plugin {
         $event->data[] = array (
             'type' => 'format',
             'title' => $this->getLang('insertcode'),
-            'icon' => '../../plugins/codebuttongs/image/code.png',
-            'open' => '<code | download>\n',
+            'icon' => '../../plugins/codebuttonmod1/image/code.png',
+            'open' => '<code | download code>\n',
             'close' => '\n</code>',
         );
+    }
+
+    /**
+     * Inserts the toolbar button
+     */
+    function insert_button_copy(& $event, $param) {
+        $event->data[] = array (
+            'type' => 'format',
+            'title' => $this->getLang('insertcodecopy'),
+            'icon' => '../../plugins/codebuttonmod1/image/copy_code_small.png',
+            'open' => '<code>\n',
+            'close' => '\n</code>',
+        );
+    }
+
+    /**
+     * Hook js script into page headers.
+     *
+     * @author Samuele Tognini <samuele@cli.di.unipi.it>
+     */
+    public function _hookjs(Doku_Event $event, $param) {
+        $event->data['script'][] = array(
+                            'type'    => 'text/javascript',
+                            'charset' => 'utf-8',
+                            '_data'   => '',
+                            'src'     => DOKU_BASE.'lib/plugins/codebuttonmod1/'.'src/codebutton.js'
+							);
+							#'src'     => DOKU_PLUGIN.'src/codebutton.js');
     }
 }
---
+++ src/codebutton.js
@@ -0,0 +1,48 @@
+/**
+ * Javascript src code for DokuWiki plugin 'codebuttonmod1'
+ * 
+ * GitHub fork https://github.com/david-it/dokuwiki_plugin_codebutton
+ * @author david-it (Davide)
+ */
+
+jQuery(document).ready( function() {
+
+	jQuery("pre").click( function() {
+		
+		ret = jQuery( this ).text();
+		
+		// Temporary modification of background
+		var bgColor = jQuery( this ).css("background-color")
+		jQuery( this ).css("background-color", "#ccc");
+		
+		// Generate a random id
+		let randomID = "id_" + Math.random().toString(36).substring(2, 15);
+		
+		// Add Overlay text
+		jQuery( this ).wrap( "<div class='" + randomID + "_wrap" + "'></div>" );
+		jQuery( this ).parent().css({"position":"relative"});
+		jQuery( this ).parent().prepend('<div class="dvCodeCopyOverlay"><small>Copied to clipboard</small></div>');
+		jQuery( ".dvCodeCopyOverlay" ).css({"position": "absolute", "left": "4px", "top":"4px", "width":"99%", "color":"grey", "background":"#f5f5f5", "border-radius":"5px", "opacity":"0.8"});
+		
+		// Add aux input to document
+		jQuery( this ).parent().append(jQuery('<textarea></textarea>').attr('id',randomID).html(ret));
+		
+		// Copy the content (original code here: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_copy_clipboard2)
+		var copyText = document.getElementById(randomID);
+		copyText.select();
+		copyText.setSelectionRange(0, 99999);
+		document.execCommand("copy");
+		
+		// Remove the aux input from document
+		jQuery( "textarea#" + randomID ).remove();
+		
+		
+		jQuery( this ).delay( 1000 ).queue(function() {
+			jQuery( this ).unwrap();
+			jQuery( this ).css("background-color", bgColor);
+			jQuery( ".dvCodeCopyOverlay" ).remove()
+		});
+
+	});
+  
+});
