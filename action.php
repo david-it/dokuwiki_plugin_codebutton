feaffdf4d1f47153b56a806e7b16bf7c0623b599
AnonTester <40003252+AnonTester@users.noreply.github.com>
2020-09-19 20:04:23 +0100

    v1.2.4: codebuttonmod1 vs codebuttonmod2 references, bugfixes

--- action.php
+++ action.php
@@ -10,7 +10,7 @@ if (!defined('DOKU_INC')) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 require_once (DOKU_PLUGIN . 'action.php');
  
-class action_plugin_codebuttonmod1 extends DokuWiki_Action_Plugin {
+class action_plugin_codebuttonmod2 extends DokuWiki_Action_Plugin {
 
     /**
      * Register the eventhandlers
@@ -19,7 +19,7 @@ class action_plugin_codebuttonmod1 extends DokuWiki_Action_Plugin {
         $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array ());
 		$controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button_copy', array ());
 		$controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button_inline', array ());
-		$controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this,'_hookjs');
+#		$controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, '_hookjs');
     }
  
     /**
@@ -29,7 +29,7 @@ class action_plugin_codebuttonmod1 extends DokuWiki_Action_Plugin {
         $event->data[] = array (
             'type' => 'format',
             'title' => $this->getLang('insertcode'),
-            'icon' => '../../plugins/codebuttonmod1/image/code.png',
+            'icon' => '../../plugins/codebuttonmod2/image/code.png',
             'open' => '<code | download>\n',
             'close' => '\n</code>',
         );
@@ -42,7 +42,7 @@ class action_plugin_codebuttonmod1 extends DokuWiki_Action_Plugin {
         $event->data[] = array (
             'type' => 'format',
             'title' => $this->getLang('insertcodeinline'),
-            'icon' => '../../plugins/codebuttonmod1/image/inline-button.png',
+            'icon' => '../../plugins/codebuttonmod2/image/inline-button.png',
             'open' => "''%%",
             'close' => "%%''",
         );
@@ -55,28 +55,12 @@ class action_plugin_codebuttonmod1 extends DokuWiki_Action_Plugin {
         $event->data[] = array (
             'type' => 'format',
             'title' => $this->getLang('insertcodecopy'),
-            'icon' => '../../plugins/codebuttonmod1/image/copy_code_small.png',
+            'icon' => '../../plugins/codebuttonmod2/image/copy_code_small.png',
             'open' => '<code>\n',
             'close' => '\n</code>',
         );
     }
     
-
-	
-    /**
-     * Hook js script into page headers.
-     *
-     * @author Samuele Tognini <samuele@cli.di.unipi.it>
-     */
-    public function _hookjs(Doku_Event $event, $param) {
-        $event->data['script'][] = array(
-                            'type'    => 'text/javascript',
-                            'charset' => 'utf-8',
-                            '_data'   => '',
-                            'src'     => DOKU_BASE.'lib/plugins/codebuttonmod1/'.'src/codebutton.js'
-							);
-							#'src'     => DOKU_PLUGIN.'src/codebutton.js');
-    }
 }
 
 ?>
